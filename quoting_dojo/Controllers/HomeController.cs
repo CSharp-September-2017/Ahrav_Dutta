12e8f38018ab4b20db9fddf9d1248ae38c1bed5d
Ahrav Dutta <ahravdutta02@gmail.com>
2017-09-23 04:41:41 -0700

    quoting dojo

--- quoting_dojo/Controllers/HomeController.cs
+++ quoting_dojo/Controllers/HomeController.cs
@@ -14,6 +14,10 @@ namespace quoting_dojo.Controllers
         [Route("")]
         public IActionResult Index()
         {
+            if(TempData["Error"] != null)
+            {
+                ViewBag.Error = TempData["Error"];
+            }
             return View();
         }
 
@@ -21,6 +25,16 @@ namespace quoting_dojo.Controllers
         [Route("submit")]
         public IActionResult SubmitForm(string name, string quote)
         {
+            if(name == null || quote == null)
+            {
+                TempData["Error"] = "Fields can not be empty";
+                return RedirectToAction("Index");
+            }
+            if(name == "" || quote == "")
+            {
+                TempData["Error"] = "Fields can not be empty";
+                return RedirectToAction("Index");
+            }
             DbConnector.Execute($"INSERT INTO Users (Name, Quote, Created_at, Updated_at) VALUES ('{name}', '{quote}', NOW(), NOW())");
             return RedirectToAction("ShowQuotes");
         }
